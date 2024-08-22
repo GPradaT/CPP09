c38945a66f5b78d29cead18ed4725344318d2f36
Guillem Prada Torres <gprada-t@car8s2.42barcelona.com>
2024-08-22 08:04:47 +0200

    empezando el main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,59 +1,19 @@
 #include "BitcoinExchange.hpp"
 
-int	main()
+int main(int argc, char *argv[])
 {
-	BitcoinExchange<float>	exchange;
-	try{
-	exchange.setCsvData();
-	exchange.printCsv();
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	/*{
-		std::cout << "\033[091mMAIN-----------1" << std::endl;
-		std::string	date = "2024-12-31";
-		std::string	date1 = "2012-02-30";
-		std::string	date2 = "2024-01-31";
-		std::string	date3 = "2009-12-30";
-		try {
-			std::cout << "=====DATE======" << std::endl;
-			std::cout << date << std::endl;
-			BitcoinExchange<int>::validDate(date);
-			std::cout << "=====DATE1=====" << std::endl;
-			std::cout << date1 << std::endl;
-			BitcoinExchange<float>::validDate(date1);
-			std::cout << "=====DATE2=====" << std::endl;
-			std::cout << date2 << std::endl;
-			BitcoinExchange<float>::validDate(date2);
-			std::cout << "=====DATE3=====" << std::endl;
-			std::cout << date3 << std::endl;
-			BitcoinExchange<int>::validDate(date3);
-		} catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
-	}
+	if (argc == 2)
 	{
-		std::cout << "\033[092mMAIN-----------2" << std::endl;
-		std::string	date = "2024-02-31";
-		std::string	date1 = "2012-02-28";
-		std::string	date2 = "2021-02-31";
-		std::string	date3 = "2009-12-30";
-		try {
-			std::cout << "=====DATE======" << std::endl;
-			std::cout << date << std::endl;
-			BitcoinExchange<int>::validDate(date);
-			std::cout << "=====DATE1=====" << std::endl;
-			std::cout << date1 << std::endl;
-			BitcoinExchange<float>::validDate(date1);
-			std::cout << "=====DATE2=====" << std::endl;
-			std::cout << date2 << std::endl;
-			BitcoinExchange<float>::validDate(date2);
-			std::cout << "=====DATE3=====" << std::endl;
-			std::cout << date3 << std::endl;
-			BitcoinExchange<int>::validDate(date3);
-		} catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
+		std::ifstream input(argv[1]);
+		if (!input.is_open())
+			return -1;
+		std::map<std::time_t, T> inputDB;
+		std::time_t	timestamp;
+		T		value;
+		while (input >> timestamp >> value)
+		{
+
 		}
-	}*/
+	}
 	return 0;
 }
